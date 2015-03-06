a2606fb72542eda709b73db90eb87e5e8c6c96ab
Lucas Silva <silval@rpi.edu>
2015-03-06 16:35:43 -0500

    display current position on map

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -4,7 +4,7 @@ Template.Dashboard.rendered = function() {
 
 Template.Dashboard.created = function() {
   // We can use the `ready` callback to interact with the map API once the map is ready.
-  GoogleMaps.ready('exampleMap', function(map) {
+  GoogleMaps.ready('dashboardMap', function(map) {
     // Add a marker to the map once it's ready
     var marker = new google.maps.Marker({
       position: map.options.center,
@@ -18,13 +18,14 @@ Template.Dashboard.events({
 });
 
 Template.Dashboard.helpers({
-  exampleMapOptions: function() {
+  dashboardMapOptions: function() {
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
       // Map initialization options
+      var position = Geolocation.latLng();
       return {
-        center: new google.maps.LatLng(-37.8136, 144.9631),
-        zoom: 8
+        center: new google.maps.LatLng(position.lat, position.lng),
+        zoom: 12
       };
     }
   }
