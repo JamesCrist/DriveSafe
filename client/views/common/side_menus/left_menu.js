dea7264ba1a59bb1ca326a8c243c2a6afb60e160
luwei0917 <luwei0917@gmail.com>
2015-04-04 14:54:38 -0400

    show error message for create group

--- client/views/common/side_menus/left_menu.js
+++ client/views/common/side_menus/left_menu.js
@@ -20,20 +20,96 @@ Template.leftMenu.events({
     });
   } ,
   'click #createGroupButton' : function (event , template) {
-    IonPopup.prompt({
+    IonPopup.show({
       title : 'Create Group' ,
-      template : 'Please enter group name' ,
-      okText : 'Create' ,
-      inputType : 'text' ,
-      inputPlaceholder : 'group name' ,
-      onOk : function (event , response) {
+      template :'<span id="inputDirections">' + 'Please enter group name' + '</span>' +'<input type="text" placeholder="group name" name="prompt" >',
+      buttons: [{
+        text: 'Create',
+        type: 'button-positive',
+        onTap: function(e,template){
 
-        Meteor.call("createNewGroup" , response , function (error) {
-          console.log(error);
-        });
-      }
+          // template ='<span>' + 'Please enter a new group name' + '</span>' +'<input type="text" placeholder="group name" name="prompt" >';
+          var inputVal =  $(template.firstNode).find('[name=prompt]').val();
+          Meteor.call("createNewGroup" , inputVal, function (error) {
+            if(error){
+              console.log($(template.firstNode).find("#inputDirections").append(error.message));
+              console.log(error.message);
+              e.preventDefault();
+            }
+            else{
+              IonPopup.close();
+            }
+          });
+        }
+      },{
+        text: 'Cancel',
+        type: 'button-positive',
+        onTap: function(e){
+          IonPopup.close();
+        }
+      }]
     });
   },
+
+  // 'click #createGroupButton' : function (event , template) {
+  //   IonPopup.prompt({
+  //     title : 'Create Group' ,
+  //     // template : 'Please enter group name' ,
+  //     template :'<span>' + 'Please enter group name' + '</span>' +'<input type="text" placeholder="group name" name="prompt" >',
+  //     okText : 'Create' ,
+  //     inputType : 'text' ,
+  //     inputPlaceholder : 'group name' ,
+  //     onOk : function (event , response) {
+  //       event.preventDefault();
+        // Meteor.call("createNewGroup" , response , function (error) {
+        //   console.log(error.message);
+        //   console.log(event);
+        //   return true
+        // });
+  //     }
+
+
+  //   });
+  // },
+
+
+  // $ionicPopup.show({
+  //   templateUrl: 'popup-template.html',
+  //   title: 'Enter Wi-Fi Password',
+  //   scope: $scope,
+  //   buttons: [
+  //     { text: 'Cancel', onTap: function(e) { return true; } },
+  //     {
+  //       text: '<b>Save</b>',
+  //       type: 'button-positive',
+  //       onTap: function(e) {
+  //         return $scope.data.wifi;
+  //       }
+  //     },
+  //   ]
+  //   }).then(function(password) {
+  //     console.log('Got wifi password', password);
+  //   });
+
+  // 'click #createGroupButton' : function (event , template) {
+  //   IonPopup.show({
+  //     title : 'Create Group' ,
+  //     template : 'Please enter group name' ,
+  //     okText : 'Create' ,
+  //     inputType : 'text' ,
+  //     inputPlaceholder : 'group name' ,
+  //     onTap : function (event , response) {
+
+
+  //       Meteor.call("createNewGroup" , response , function (error) {
+  //         console.log(error.message);
+
+  //       });
+  //     }
+
+  //   });
+  // },
+
   'click #leaveGroup' : function(event, template) {
     IonPopup.confirm({
       title: 'Are you sure?',
@@ -50,3 +126,12 @@ Template.leftMenu.events({
     });
   }
 });
+
+
+var showError = function(error) {
+    IonPopup.alert({
+      title: 'An Alert',
+      template: 'This is an alert!',
+      okText: 'Got It.'
+    });
+}
