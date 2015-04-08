6e42bccd6994f52661a3c862dc823efead1fe735
taboadakt <taboadakt@gmail.com>
2015-04-08 10:44:45 -0400

    request menu

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -35,11 +35,7 @@ Template.mapCanvas.rendered = function() {
 Template.Dashboard.created = function() {
 };
 
-Template.Dashboard.events({
-  "click #button": function() {
-    alert("Ride Requested!");
-  }
-});
+
 
 Template.Dashboard.helpers({
   errorMessage: function() {
@@ -48,5 +44,33 @@ Template.Dashboard.helpers({
   mapIsLoaded: function() {
     pageSession.set("location", Geolocation.latLng());
     return pageSession.get("location") != null;
+  }
+});
+
+Template.request_modal.events({
+  "focus #pac-input": function() {
+    src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places";
+    function initialize() {
+      var input = /** @type {HTMLInputElement} */(
+          document.getElementById('pac-input'));
+
+      var autocomplete = new google.maps.places.Autocomplete(input);
+     
+      google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+          return;
+        }
+        var address = '';
+        if (place.address_components) {
+          address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+        }
+      });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
   }
 });
