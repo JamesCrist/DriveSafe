587c34e3ceea4d97fbdfeebfaeb6d5eb826f1d79
luwei0917 <luwei0917@gmail.com>
2015-04-20 19:26:41 -0400

    minor update

--- update_document.py
+++ update_document.py
@@ -11,5 +11,6 @@ f = open("jsdoc_address_temp","r")
 jsdoc = f.readline().rstrip('\n')
 f.close()
 
-os.system("cd ./source \n export DOCS_PATH=../docs \n git grep -al \"@summary\" | xargs -L 10000 -t  "+ node + " " + jsdoc + " -t ./meteor-jsdoc/template  -c ./meteor-jsdoc/jsdoc-conf.json"  )
-
+os.system("cd ./source \n export DOCS_PATH=../docs \n git grep -al \"@summary\" | xargs -L 10000 -t  "+ node + " " + jsdoc + " -t ../meteor-jsdoc/template  -c ../meteor-jsdoc/jsdoc-conf.json"  )
+os.system("rm node_address_temp")
+os.system("rm jsdoc_address_temp")
