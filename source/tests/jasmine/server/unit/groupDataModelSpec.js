826e08580bd177f3b1ee031277eea3a0a7d7c57e
Lucas Silva <silval@rpi.edu>
2015-04-20 18:55:04 -0400

    moved tests into source directory

---
+++ source/tests/jasmine/server/unit/groupDataModelSpec.js
@@ -0,0 +1,28 @@
+"use strict";
+describe("Group", function () {
+  it("should be created with admin, name, members, and drivers", function () {
+    spyOn(Groups, "insert").and.callFake(function(doc, callback) {
+      // simulate async return of id = "1";
+      callback(null, "1");
+    });
+
+    var group = new Group(null, "Group 1", "1", null, null);
+
+    expect(group.name).toBe("Group 1");
+    expect(group.admin).toBe("1");
+    expect(group.members).toEqual(["1"]);
+    expect(group.drivers).toEqual([]);
+
+    group.save();
+
+    // id should be defined
+    expect(group.id).toEqual("1");
+
+    expect(Groups.insert).toHaveBeenCalledWith({
+      name: "Group 1",
+      admin: "1",
+      members: ["1"],
+      drivers: []
+    }, jasmine.any(Function));
+  });
+});
